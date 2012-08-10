92b46b11e70c3ca391845111b8363fdc47a5af5d
Steven Shiau <steven@nchc.org.tw>
2012-08-10 13:48:59 +0800

    Read version number from debian/changelog, instead of rpm spec.

--- toolbox/make-deb.sh
+++ toolbox/make-deb.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # Steven Shiau <steven _at_ nchc org tw)
-#
+
+# Settings
+PKG="drbl"
 
 set -e
-PKG="drbl"
+
 #
-VER="$(LC_ALL=C head -n 1 debian/changelog  | grep -i "^drbl" | grep -E -o "\(.*\)" | sed -r -e "s/\(//g" -e "s/\)//g" | cut -d"-" -f1)"
+VER="$(LC_ALL=C head -n 1 debian/changelog  | grep -i "^${PKG}" | grep -E -o "\(.*\)" | sed -r -e "s/\(//g" -e "s/\)//g" | cut -d"-" -f1)"
 [ -z "$VER" ] && echo "No version found in debian/changelog! Program terminated!"
 echo "VER: $VER"
 
@@ -14,14 +16,14 @@ TARBALL=${PKG}-${VER}.tar.bz2
 TARBALL_ORIG=${PKG}_${VER}.orig.tar.bz2
 
 # check
-[ ! -f "$TARBALL" ] && echo "Can NOT find file $TARBALL! Did you forget to update the rdate in file drbl.spec ? Program Stop!!!" && exit 1
+[ ! -f "$TARBALL" ] && echo "Can NOT find file $TARBALL! Program Stop!!!" && exit 1
 
 # mkdir for build
-rm -rf debforge 
+rm -rf debforge
 mkdir debforge
 (cd debforge; ln -fs ../$TARBALL $TARBALL_ORIG)
 tar -xvjf $TARBALL -C debforge/
-cp -a debian debforge/drbl-$VER/
-cd debforge/drbl-$VER
+cp -a debian debforge/$PKG-$VER/
+cd debforge/$PKG-$VER
 debuild --no-tgz-check --no-lintian
 rm -f $TARBALL_ORIG
--- toolbox/packit.sh
+++ toolbox/packit.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 # generate the ChangeLog.txt and create the tarball
 
+# Settings
+PKG="clonezilla"
+FILES_DIRS="Makefile drbl.spec sbin bin doc lang pkg scripts themes conf image pki setup prerun postrun"
+
 set -e
 #
-VER="$(LC_ALL=C head -n 1 debian/changelog  | grep -i "^drbl" | grep -E -o "\(.*\)" | sed -r -e "s/\(//g" -e "s/\)//g" | cut -d"-" -f1)"
+VER="$(LC_ALL=C head -n 1 debian/changelog  | grep -i "^${PKG}" | grep -E -o "\(.*\)" | sed -r -e "s/\(//g" -e "s/\)//g" | cut -d"-" -f1)"
 [ -z "$VER" ] && echo "No version found in debian/changelog! Program terminated!"
 echo "VER: $VER"
 
-#
+# Create the ChangeLog file
 cat <<EOF > doc/ChangeLog.txt
 DRBL project.
-Author: Steven Shiau <steven _at_ nchc org tw>, Blake, Kuo-Lien Huang (klhaung _at_ gmail com), Ceasar Sun (ceasar _at_ nchc org tw), Jazz Wang (jazz _at_ nchc org tw) and Thomas Tsai (thomas _at_ nchc org tw)
+Authors: Steven Shiau <steven _at_ nchc org tw>, Blake, Kuo-Lien Huang (klhaung _at_ gmail com), Ceasar Sun (ceasar _at_ nchc org tw), Jazz Wang (jazz _at_ nchc org tw) and Thomas Tsai (thomas _at_ nchc org tw)
 License: GPL
 http://drbl.org
 http://drbl.sourceforge.net
@@ -20,14 +24,15 @@ EOF
 
 cat debian/changelog >> doc/ChangeLog.txt
 
+td="${PKG}-${VER}"
+
 #
-td=drbl"-""$VER"
 [ -d "$td" ] && rm -rf $td
 mkdir -p $td
 # Clean stale files in debian
 rm -rf debian/{drbl,tmp}
-cp -ar Makefile drbl.spec sbin bin doc lang pkg scripts themes conf image pki setup prerun postrun $td/
+cp -ar $FILES_DIRS $td/
 
-echo $VER > $td/doc/VERSION.$VER
+echo $VER > $td/doc/VERSION
 tar cvjf $td.tar.bz2 --owner=root --group=root $td
 rm -rf $td
